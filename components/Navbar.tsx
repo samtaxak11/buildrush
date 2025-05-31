84c05662f1a4c05647ffdfacd0a0d542ac1f9106
Sourabh Takshak <takshaksourabh11@gmail.com>
2025-06-01 03:40:16 +0530

    Updated Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { Menu, X } from "lucide-react";
-import Image from "next/image";
+import { Menu, X, CircleIcon } from "lucide-react";
 
 interface NavbarProps {
   isScrolled: boolean;
@@ -24,13 +23,8 @@ const Navbar = ({ isScrolled }: NavbarProps) => {
     >
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
-          <Image
-            src="/build-removebg-preview.png"
-            alt="Build Logo"
-            width={100}
-            height={40}
-            className="h-8 w-auto"
-          />
+          <CircleIcon className="h-8 w-8 text-[#B6FF40]" />
+          <span className="font-playfair text-xl font-bold text-white">Simple<span className="text-[#B6FF40]">Suite</span></span>
         </Link>
 
         {/* Mobile menu button */}
@@ -62,11 +56,10 @@ const Navbar = ({ isScrolled }: NavbarProps) => {
 
         {/* CTA Button */}
         <Link 
-          href="https://tally.so/r/m6y4xA" 
-          target="_blank"
+          href="#pricing" 
           className="hidden lg:flex items-center border border-[#B6FF40] text-[#B6FF40] hover:bg-[#B6FF40]/10 rounded-full px-6 py-2 transition-all duration-300"
         >
-          Share your Idea
+          See Plans
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2">
             <path d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z" fill="#B6FF40"/>
           </svg>
@@ -113,12 +106,11 @@ const Navbar = ({ isScrolled }: NavbarProps) => {
               Course
             </Link>
             <Link 
-              href="https://tally.so/r/m6y4xA"
-              target="_blank"
+              href="#pricing" 
               className="flex items-center justify-center bg-[#B6FF40] text-[#0B0B0B] font-medium rounded-full px-6 py-2"
               onClick={() => setIsMenuOpen(false)}
             >
-              Share your Idea
+              See Plans
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2">
                 <path d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z" fill="#0B0B0B"/>
               </svg>
